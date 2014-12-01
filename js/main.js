1f93be64bb0cb2e2ef9f3e9c2166fa22496cd7ba
Stanisław Wasiutyński <staszek.wasiutynski@gmail.com>
2014-12-01 23:48:17 +0100

    + displaying results

--- js/main.js
+++ js/main.js
@@ -1,13 +1,60 @@
 $(function(){
     $('#search').click(function(){
 
-        var query = $('#q').val()
+        var q = $('#q').val()
 
-        $.get('http://localhost:9200/sdm/_search', {
-            q: query
-        }, function(resp){
-            console.log(resp)
-        }, 'jsonp')
+        var query = {
+            query: {
+                match: {
+                    _all: q
+                }
+            },
+            highlight : {
+                fields : {
+                    experience: {},
+                    interests: {}
+                }
+            }
+        }
 
+        $.ajax({
+            url: "http://localhost:9200/sdm/_search",
+            type: "POST",
+            data: JSON.stringify(query),
+            contentType: 'application/json',
+        }).then(function(resp){
+            var results = []
+            resp.hits.hits.forEach(function(hit){
+                var model = new Model(hit)
+                results.push(new Result({model: model}).render().el)
+            })
+            $('.results > .row > div:first-child').html(results)
+        })
+
+    })
+
+    var Model = Backbone.Model.extend({
+        display_name: function() {
+            var source = this.get('_source')
+            return [source.first_name, source.last_name].join(' ')
+        },
+        highlights: function() {
+            var all = this.get('highlight')
+            var result = []
+            return _.map(_.values(all), function(hl){
+                return hl.join(', ')
+            }).join(' &hellip; ')
+        }
+    })
+
+    var Result = Backbone.View.extend({
+        className: 'result',
+        template: _.template("<a href='#'><%= display_name() %></a><div><%= highlights() %></div>"),
+        initialize: function() { },
+        render: function() {
+            console.log(this.model)
+            this.$el.html(this.template(this.model))
+            return this
+        }
     })
 })
